01a57842c321b61e952be24fd5e3ddcedde74431
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-04-02 14:32:46 +0000

    .

--- script.js
+++ script.js
@@ -15,4 +15,13 @@ diminuiFonteBotao.addEventListener('click', function () {
     document.body.style.fontSize = `${tamanhoFonteAtual}rem`
 })
 
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+    })
+
 } )
